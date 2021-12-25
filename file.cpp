36ede117f105e74e2f1748762bd7cc82ac8b0996
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-25 23:24:34 +1100

    perf(file.cpp): major improvements

--- file.cpp
+++ file.cpp
@@ -3,19 +3,55 @@
 
 using namespace std;
 
-// Takes an input size from the user and writes a text file with that size
-int main() {
-    int size, i = 0;
-    ofstream file;
-
-    cout << "Enter your desired file size (bytes): ";
-    cin >> size;
-
-    file.open("file");
-    while (i < size) {
-        file << "a";
+string str(int size)
+{
+    int i = 0;
+    string s;
+    while (i < size)
+    {
+        s += 'a';
         i++;
     }
+    return s;
+};
+
+// Takes an input size from the user and writes a text file with that size
+int main()
+{
+    string mb = str(1024 * 1024);
+
+    cout << "Enter your desired file size (bytes): ";
+    int size;
+    cin >> size;
+
+    cout << "Enter your desired file name (file): ";
+    string fileName;
+    if (fileName.empty())
+    {
+        fileName = "file";
+    }
+    else
+    {
+        cin >> fileName;
+    }
+
+    string rem = str(size % 1000000);
+    size = size / 1000000;
+
+    ofstream file;
+
+    file.open(fileName);
+
+    int i = 0;
+
+    while (i < size)
+    {
+        file << mb;
+        i++;
+    }
+
+    file << rem;
+
     file.close();
 
     return 0;
