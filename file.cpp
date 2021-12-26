0e6f83fdc49e82b2c390f6004f341e9a15215237
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-26 12:26:46 +1100

    fix: fixed issues with empty file names and reading from cin;

--- file.cpp
+++ file.cpp
@@ -3,40 +3,42 @@
 
 using namespace std;
 
-string str(int size)
+void nStr(int size, char *outStr)
 {
-    int i = 0;
-    string s;
-    while (i < size)
+    char str[size];
+
+    for (int i = 0; i < size; ++i)
     {
-        s += 'a';
-        i++;
+        outStr[i] = str[i];
     }
-    return s;
 };
 
 // Takes an input size from the user and writes a text file with that size
 int main()
 {
-    string mb = str(1024 * 1024);
+    char mb[1024 * 1024];
+    nStr(1024 * 1024, mb);
 
     cout << "Enter your desired file size (bytes): ";
     int size;
-    cin >> size;
+    string sizeInput;
+    getline(cin, sizeInput);
+    size = stoi(sizeInput);
 
     cout << "Enter your desired file name (file): ";
     string fileName;
+    getline(cin, fileName);
     if (fileName.empty())
     {
         fileName = "file";
     }
-    else
-    {
-        cin >> fileName;
-    }
 
-    string rem = str(size % 1000000);
-    size = size / 1000000;
+    cout << "Made it here" << endl;
+
+    char rem[size % 1000000];
+    nStr(size % 1000000, rem);
+
+    cout << rem;
 
     ofstream file;
 
@@ -44,7 +46,7 @@ int main()
 
     int i = 0;
 
-    while (i < size)
+    while (i < size / 1000000)
     {
         file << mb;
         i++;
