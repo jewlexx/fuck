a49efa10356f3ba31771b5b8aa5e8003291c80a8
jamesinaxx <grassojames5@gmail.com>
2022-02-28 11:19:39 +1100

    added files

---
+++ rust/genfile/target/debug/deps/genfile.d
@@ -0,0 +1,5 @@
+C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\deps\genfile.exe: src\main.rs
+
+C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\deps\genfile.d: src\main.rs
+
+src\main.rs:
---
+++ rust/genfile/target/debug/genfile.d
@@ -0,0 +1,1 @@
+C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\genfile.exe: C:\Users\jgrass23\dev\fuck\rust\genfile\src\main.rs
