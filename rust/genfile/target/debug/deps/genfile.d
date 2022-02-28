1c633f2ba8e96750fc48dfe946374ae48ecbda1b
jamesinaxx <grassojames5@gmail.com>
2022-02-28 11:20:17 +1100

    fixed gitignore

--- rust/genfile/target/debug/deps/genfile.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\deps\genfile.exe: src\main.rs
-
-C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\deps\genfile.d: src\main.rs
-
-src\main.rs:
--- rust/genfile/target/debug/genfile.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jgrass23\dev\fuck\rust\genfile\target\debug\genfile.exe: C:\Users\jgrass23\dev\fuck\rust\genfile\src\main.rs
