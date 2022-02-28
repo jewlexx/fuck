b09d376de1fbf4738eaa211d66a63b599af933e6
jamesinaxx <grassojames5@gmail.com>
2022-02-28 11:30:24 +1100

    added ability to write a single byte

--- rust/genfile/src/main.rs
+++ rust/genfile/src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+    env,
+    fs::{self},
+    io::Write,
+};
+
+const BYTE: [u8; 1] = [0];
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = env::current_dir().unwrap();
+    path.push("file");
+
+    let mut file = if !path.exists() {
+        fs::File::create(&path).unwrap()
+    } else {
+        fs::File::open(&path).unwrap()
+    };
+
+    file.write_all(&BYTE).unwrap();
 }
