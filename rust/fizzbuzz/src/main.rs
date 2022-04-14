fda906d614b67a9750943422ad68d8f2ecc5af33
Juliette Cordor <grassojames5@gmail.com>
2022-04-14 14:22:16 +1000

    fixed rust fizzbuzz

--- rust/fizzbuzz/src/main.rs
+++ rust/fizzbuzz/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
     for i in 0..=100 {
         let mut output = String::new();
+
         if i % 3 == 0 {
             output += "Fizz";
         }
+
         if i % 5 == 0 {
             output += "Buzz";
         }
+
         if output.is_empty() {
             println!("{}", i)
         } else {
