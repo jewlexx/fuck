532488c52ddb62be44a1c0038b6cfa75a0bea93a
Juliette Cordor <grassojames5@gmail.com>
2022-05-30 19:42:16 +1000

    haha

---
+++ c/huh.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int y = 2 / 4;
+
+    if (y == 0) {
+        printf("I was told that in maths if the number was larget than .5 it would round UP not DOWN wtf C???");
+    }
+
+    return 0;
+}
